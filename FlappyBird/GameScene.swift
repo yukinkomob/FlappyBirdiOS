007db1dd571e44afebd87a8b52b06d2a342bd51a
Yuki Nishimura <yukinkopen@gmail.com>
2021-11-23 22:07:12 +0900

    SKViewとSKSceneを追加

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Mac on 2021/11/23.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+    }
+}
--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -5,13 +5,18 @@
 //  Created by Mac on 2021/11/23.
 //
 
-import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        let scene = GameScene(size:skView.frame.size)
+        skView.presentScene(scene)
     }
 
 
