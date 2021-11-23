a18739ed7b395e83451d89173df4879d48f87717
Yuki Nishimura <yukinkopen@gmail.com>
2021-11-23 22:34:51 +0900

    スプライトの表示を追加

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -11,5 +11,17 @@ class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+        
+        let groundTexture = SKTexture(imageNamed: "ground")
+        groundTexture.filteringMode = .nearest
+        
+        let groundSprite = SKSpriteNode(texture: groundTexture)
+        
+        groundSprite.position = CGPoint(
+            x: groundTexture.size().width / 2,
+            y: groundTexture.size().height / 2
+        )
+        
+        addChild(groundSprite)
     }
 }
