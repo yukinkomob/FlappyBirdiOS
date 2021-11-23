3c9e66a7c9985968b740224d3e80bee8857c4550
Yuki Nishimura <yukinkopen@gmail.com>
2021-11-23 23:30:14 +0900

    地面のスクロール対応を追加

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -9,11 +9,35 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    var scrollNode: SKNode!
+    
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
         
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = .nearest
+        
+        let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
+        
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5)
+        
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0)
+        
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        for i in 0..<needNumber {
+            let sprite = SKSpriteNode(texture: groundTexture)
+            
+            sprite.position = CGPoint(
+                x: groundTexture.size().width / 2 + groundTexture.size().width * CGFloat(i),    y: groundTexture.size().height / 2
+            )
+            
+            sprite.run(repeatScrollGround)
+            scrollNode.addChild(sprite)
+        }
         
         let groundSprite = SKSpriteNode(texture: groundTexture)
         
